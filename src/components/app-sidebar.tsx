50b50a784555c9992d500e768aeb7e8d35950966
Uttam Rao <uttamrao2000@gmail.com>
2025-06-07 00:28:55 +0530

    Some more update in sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,15 @@
-import { Calendar, ChevronUp, Home, Inbox, Search, Settings, User2 } from "lucide-react"
+import {
+  Home,
+  Users,
+  ChevronRight,
+  ChevronDown,
+  User2,
+  Calendar,
+  Inbox,
+  Search,
+  Settings,
+  University,
+} from "lucide-react"
 
 import {
   Sidebar,
@@ -11,99 +22,124 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
 
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-]
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export function AppSidebar() {
-  // const {
-  //   state,
-  //   open,
-  //   setOpen,
-  //   openMobile,
-  //   setOpenMobile,
-  //   isMobile,
-  //   toggleSidebar,
-  // } = useSidebar()
   return (
     <Sidebar side="left" variant="sidebar" collapsible="icon">
-      <SidebarHeader>Vidyalaya Sanchalak</SidebarHeader>
+      <SidebarHeader>
+        <SidebarMenu>
+      <SidebarMenuItem>
+          <SidebarMenuButton asChild>
+                  <a href="#">
+                    <University />
+                    <span>Vidyalaya Sanchalak</span>
+                  </a>
+                </SidebarMenuButton>
+      </SidebarMenuItem>
+      </SidebarMenu>
+        </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
+              {/* Not Collapsible: Dashboard */}
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href="#">
+                    <Home />
+                    <span>Dashboard</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+
+              {/* Collapsible: Students */}
+              <Collapsible defaultOpen className="group/collapsible">
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      <Users />
+                      <span>Students</span>
+                      <ChevronDown className="ml-auto group-data-[state=closed]/collapsible:inline group-data-[state=open]/collapsible:hidden" />
+                      <ChevronRight className="ml-auto group-data-[state=open]/collapsible:inline group-data-[state=closed]/collapsible:hidden" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
                 </SidebarMenuItem>
-              ))}
+
+                <CollapsibleContent className="pl-4">
+                  <SidebarMenuSub>
+  <SidebarMenuSubItem>
+    <SidebarMenuButton asChild>
+      <a href="#">All Students</a>
+    </SidebarMenuButton>
+  </SidebarMenuSubItem>
+  <SidebarMenuSubItem>
+    <SidebarMenuButton asChild>
+      <a href="#">Add Student</a>
+    </SidebarMenuButton>
+  </SidebarMenuSubItem>
+  <SidebarMenuSubItem>
+    <SidebarMenuButton asChild>
+      <a href="#">Edit Student</a>
+    </SidebarMenuButton>
+  </SidebarMenuSubItem>
+  <SidebarMenuSubItem>
+    <SidebarMenuButton asChild>
+      <a href="#">Student Attendance</a>
+    </SidebarMenuButton>
+  </SidebarMenuSubItem>
+</SidebarMenuSub>
+
+                </CollapsibleContent>
+              </Collapsible>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+
       <SidebarFooter>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <SidebarMenuButton>
-                    <User2 /> Admin
-                    <ChevronUp className="ml-auto" />
-                  </SidebarMenuButton>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  side="top"
-                  className="w-[--radix-popper-anchor-width]"
-                >
-                  <DropdownMenuItem>
-                    <span>Account</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Billing</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <span>Sign out</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> Admin
+                  <ChevronDown className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   )
 }
